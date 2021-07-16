96fbf22f868887d96efe8258fec7f9db026fd563
Stevemwa <62056954+Stevemwa@users.noreply.github.com>
2021-07-16 11:06:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function (sunriceupdate) {
+  var x=0;
+  var e=document.getElementById('sunroom');
+  var y=parseFloat(e)+x;
+  document.getElementById('sunriserooms'),innerHTML="SUNRISE HOSTEL"+"    " + "Great security,spacious room."+
+  y + "rooms available.";
+
+}
